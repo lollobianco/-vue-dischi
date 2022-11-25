b5bb97cdef71a6275c250c471e9d594839bd1919
lollobianco <loreb2002@gmail.com>
2022-11-25 13:46:40 +0100

    Creata struttura di partenza  e importata API con axios

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+
+    <HeaderComp/>
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import HeaderComp from './components/HeaderComp.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HeaderComp
+  },
+  data() {
+      return {
+        albums: ''
+      }
+    },
+    mounted() {
+  
+      this.getAlbums();
+
+    },
+    methods: {
+
+      getAlbums() {
+
+        axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+          .then((response) => {
+            this.albums = response.data
+          })
+          
+      }
+    }
+}
+</script>
+
+<style lang="scss">
+#app {
+  height: 100vh;
+  background-color: #1E2D3B;
+}
+</style>
---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="header-component">
+
+    
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderComp',
+  props: {
+    
+  }
+}
+</script>
+
+
+<style scoped lang="scss">
+
+#header-component{
+  height: 10%;
+  background-color: #2D3A46;
+}
+
+</style>

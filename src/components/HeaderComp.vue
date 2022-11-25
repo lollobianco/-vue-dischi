667c7e831161aceb15d7a6dbe6dc0fc651c33b3f
lollobianco <loreb2002@gmail.com>
2022-11-25 17:55:56 +0100

    Aggiunto logo

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="header-component">
 
-
+    <div id="logo-container">
+      <img src="../assets/logo-small.svg" alt="">
+    </div>
+    
     
   </div>
 </template>
@@ -21,6 +24,21 @@ export default {
 #header-component{
   height: 60px;
   background-color: #2D3A46;
+}
+
+#logo-container{
+  height: 60px;
+  width: 60px;
+  background-color: #2D3A46;
+  display: flex;
+  align-items: center;
+  margin-left: 15px;
+
+  img{
+    background-color: #2D3A46;
+    width: 80%;
+  }
+
 }
 
 </style>

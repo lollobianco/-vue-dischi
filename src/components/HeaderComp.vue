218423a55fb0669c3002366c959f440746daa21d
lollobianco <loreb2002@gmail.com>
2022-11-28 15:24:01 +0100

    Aggiunta select

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -4,6 +4,15 @@
     <div id="logo-container">
       <img src="../assets/logo-small.svg" alt="">
     </div>
+
+    <select class="form-select form-select-sm" aria-label=".form-select-sm example">
+      <option selected>Filter by musical genres</option>
+      <option value="1">All</option>
+      <option value="2">Rock</option>
+      <option value="3">Pop</option>
+      <option value="4">Jazz</option>
+      <option value="5">Metal</option>
+    </select>
     
     
   </div>
@@ -24,6 +33,8 @@ export default {
 #header-component{
   height: 60px;
   background-color: #2D3A46;
+  display: flex;
+  align-items: center;
 }
 
 #logo-container{
@@ -39,6 +50,11 @@ export default {
     width: 80%;
   }
 
+}
+
+.form-select{
+  width: 20%;
+  margin-left: 20px;
 }
 
 </style>
